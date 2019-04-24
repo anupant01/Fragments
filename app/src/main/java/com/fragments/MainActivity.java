37ef7011fd7bb020463851a0fdbc0b4c0eb02a2d
anupant01 <pantaanupa@gmail.com>
2019-04-24 10:27:06 +0545

    button onclick listener

--- app/src/main/java/com/fragments/MainActivity.java
+++ app/src/main/java/com/fragments/MainActivity.java
@@ -1,9 +1,14 @@
 package com.fragments;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import fragments.FirstFragment;
+import fragments.SecondFragment;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,6 +27,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (status){
+            FirstFragment firstFragment =new FirstFragment();
+            fragmentTransaction.replace(R.id.fragmentscontainer,firstFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            btnfragment.setText("Load Second Fragment");
+            status=false;
+        }
+
+        else {
+
+            SecondFragment secondFragment=new SecondFragment();
+            fragmentTransaction.replace(R.id.fragmentscontainer,secondFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            btnfragment.setText("Load First Fragment");
+            status=true;
+
+        }
+
+
+
+
 
 
      
