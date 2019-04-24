8cde2ab78f8b1e542def1502bbd435de11761791
anupant01 <pantaanupa@gmail.com>
2019-04-24 11:19:30 +0545

    two number added in fragments

--- app/src/main/java/fragments/FirstFragment.java
+++ app/src/main/java/fragments/FirstFragment.java
@@ -6,13 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.fragments.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment  implements View.OnClickListener{
+
+    private Button btncalculate;
+    private EditText etfirst, etsecond;
 
 
     public FirstFragment() {
@@ -24,7 +30,28 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+         View view= inflater.inflate(R.layout.fragment_first, container, false);
+
+         etfirst=view.findViewById(R.id.etfirst);
+         etsecond=view.findViewById(R.id.etsecond);
+         btncalculate=view.findViewById(R.id.btncalculate);
+
+
+         btncalculate.setOnClickListener(this);
+         return view;
+
     }
 
+    @Override
+    public void onClick(View v) {
+
+        int first= Integer.parseInt(etfirst.getText().toString());
+        int second=Integer.parseInt(etsecond.getText().toString());
+
+        int result = first + second;
+
+
+        Toast.makeText(getActivity(),"Sum is: "+ result, Toast.LENGTH_SHORT).show();
+
+    }
 }
